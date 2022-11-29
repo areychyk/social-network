e3e6dce73d6a1d3910448d2b4dc291f8dec016ca
506-4 <506-4@BELDORNII.LOCAL>
2022-11-29 13:45:06 +0300

    page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,54 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     good buy, samurai!
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <header className="header">
+                <img
+                    src={'https://cdn.dribbble.com/userupload/3158902/file/original-7c71bfa677e61dea61bc2acd59158d32.jpg?resize=400x0'}/>
+            </header>
+            <nav className="nav">
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className="content">
+                <div>
+                    <img
+                        src={'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'}/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    my posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+
+                Main content
+            </div>
+        </div>
+    );
 }
 
 export default App;

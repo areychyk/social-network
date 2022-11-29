106eadd8482e807930aa40589fe76242e5acd6c1
506-4 <506-4@BELDORNII.LOCAL>
2022-11-29 16:13:30 +0300

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 function App() {
     return (
         <div className="app-wrapper">
-            <header className="header">
-                <img
-                    src={'https://cdn.dribbble.com/userupload/3158902/file/original-7c71bfa677e61dea61bc2acd59158d32.jpg?resize=400x0'}/>
-            </header>
-            <nav className="nav">
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-            </nav>
-            <div className="content">
-                <div>
-                    <img
-                        src={'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'}/>
-                </div>
-                <div>
-                    ava + description
-                </div>
-                <div>
-                    my posts
-                    <div>
-                        New post
-                    </div>
-                    <div>
-                        post 1
-                    </div>
-                    <div>
-                        post 2
-                    </div>
-                </div>
+            <Header/>
+            <Navbar/>
+            <Profile/>
 
-                Main content
-            </div>
         </div>
     );
 }

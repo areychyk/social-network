7e2f018d14343fe2ccd32036dcdf6b4cb299c5ac
Ariiychyk Denis <areychyk@mail.ru>
2022-12-06 21:52:41 +0300

    props

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,17 +3,20 @@ import React from 'react';
 import s from '../MyPosts/MyPosts.module.css'
 import Post from "./Post/Post";
 
+
+
 const MyPosts = () => {
+
     return (
         <div>
             <div>
                 <textarea></textarea>
                 <button>Add post</button>
             </div>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
+            <Post message={'message1'} like={15}/>
+            <Post message={'message2'} like={20}/>
+
+
 
         </div>
 
